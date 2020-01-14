71109a42be00d1c5fefc81c138714a5c3874f80f
LeuAlmeida <leo@webid.net.br>
2020-01-14 11:31:39 -0300

    chore: Remove reject unauthorized from routes file

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 const Feed = require('rss-to-json');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const routes = new Router();
 
 routes.get('/news', (req, res) => Feed.load('https://metodista.br/rss.xml', (err, rss) => res.json(rss)));
