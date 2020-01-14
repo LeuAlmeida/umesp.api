69dccbe1f0cdc401461435a0e8bc3f08e51cab51
LeuAlmeida <leo@webid.net.br>
2020-01-14 11:45:57 -0300

    chore: Improving app file using sentry monitor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
+import * as Sentry from '@sentry/node';
+
+import sentryConfig from './config/sentry';
 
 import routes from './routes';
 
@@ -7,17 +10,34 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    // this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.requestHandler());
   }
+
+  // exceptionHandler() {
+  //   this.server.use(async (err, req, res, next) => {
+  //     if (process.env.NODE_ENV === 'development') {
+  //       const errors = await new Youch(err, req).toJSON();
+
+  //       return res.status(500).json(errors);
+  //     }
+  //     return res.status(500).json({ error: 'Internal server error.' });
+  //   });
+  // }
 }
 
 export default new App().server;
