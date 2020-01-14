a341318179b9bfd451478e1c4a7486b0901e25f1
LeuAlmeida <leo@webid.net.br>
2020-01-14 11:22:37 -0300

    feat: Create rss transpile to json at '/news' route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 
+const Feed = require('rss-to-json');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const routes = new Router();
 
-routes.get('/news', (req, res) => res.json({ ok: 'true' }));
+routes.get('/news', (req, res) => Feed.load('https://metodista.br/rss.xml', (err, rss) => res.json(rss)));
 
 export default routes;
