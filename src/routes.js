f4e3818dcd78cab0837829c738de8ea3daf9949c
LeuAlmeida <leo@webid.net.br>
2020-01-14 10:44:53 -0300

    chore: Improving routes using ESLint rules

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/news', (req, res) => {
-  return res.send('Hello World')
-} )
+  return res.json({ ok: 'true' });
+});
+
 export default routes;
