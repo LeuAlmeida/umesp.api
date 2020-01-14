27d665c9d190aff007cb7bbab648a5905c4343ae
LeuAlmeida <leo@webid.net.br>
2020-01-14 11:55:00 -0300

    refactor: Improving app using cors middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 
+import cors from 'cors';
 import express from 'express';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
@@ -20,7 +21,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
-
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
